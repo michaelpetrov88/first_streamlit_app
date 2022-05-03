e3fcf1e64591fe4049e1577441cf7e9110b65a23
michaelpetrov88 <104067090+michaelpetrov88@users.noreply.github.com>
2022-05-03 09:08:24 +0100

    Update streamlit_app.py
    adding the code format and input change options

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,15 +32,12 @@ streamlit.header('fruityvice fruit advice')
 try :
  fruit_choice = streamlit.text_input('What fruit would you like information about?' , 'Kiwi') 
  if not fruit_choice: 
-streamlit.error("Please select a fruit to get information.")
+    streamlit.error("Please select a fruit to get information.")
  else: 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 # streamlit.text(fruityvice_response.json()) just writes the data to the screen
-
-# takes json advice and normalizes 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output to screen
-streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e: 
 streamlit.error()
